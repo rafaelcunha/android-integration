e4dd45f5d341cdb0d617d903c01400ec8f21a13f
Sebastian Dominguez <sedominguez78@gmail.com>
2016-02-22 10:59:55 -0300

    Initial Commit

---
+++ app/src/main/java/com/mercadopago/point/integration/basic/Main.java
@@ -0,0 +1,116 @@
+package com.mercadopago.point.integration.basic;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.pgallazzi.apporonga.R;
+
+import java.util.List;
+
+public class Main extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+
+    public static final String LAUNCHER_INTENT_DESCRIPTION = "launcher_intent_description";
+    public static final String LAUNCHER_INTENT_AMOUNT = "launcher_intent_amount";
+    public static final String LAUNCHER_INTENT_TYPE = "launcher_intent_type";
+    public static final String LAUNCHER_INTENT_INSTALLMENTS = "launcher_intent_installments";
+
+    public static final int PAYMENT_REQUEST = 1;
+
+    EditText reference;
+    EditText amount;
+    EditText installments;
+    FloatingActionButton go;
+
+    String cc_selected;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        reference = (EditText) findViewById(R.id.reference);
+        amount = (EditText) findViewById(R.id.amount);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.debit_credit);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cc_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+        installments = (EditText) findViewById(R.id.installments);
+        go = (FloatingActionButton) findViewById(R.id.go);
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent();
+                i.setAction("com.mercadopago.PAYMENT_ACTION");
+                // Amount of transaction
+                i.putExtra(LAUNCHER_INTENT_AMOUNT, amount.getText().toString());
+                // Description of transaction
+                i.putExtra(LAUNCHER_INTENT_DESCRIPTION, reference.getText().toString());
+                if (spinner.getSelectedItemPosition() == 0) {
+                    cc_selected = "credit_card";
+                } else {
+                    cc_selected = "debit_card";
+                }
+                // Payment type of transaction ( credit_card | debit_card  )
+                i.putExtra(LAUNCHER_INTENT_TYPE, cc_selected);
+                // # of installments
+                i.putExtra(LAUNCHER_INTENT_INSTALLMENTS, installments.getText().toString());
+                // Before we can start the intent, we should check if this phone handle the intent?
+                if (isAvailable(i)) {
+                    // start activity for result
+                    startActivityForResult(i, PAYMENT_REQUEST);
+                } else {
+                    // send to google play.
+                    try {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName())));
+                    } catch (ActivityNotFoundException e) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://play.google.com/store/apps/details?id=" + getPackageName())));
+                    }
+                }
+
+            }
+        });
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        parent.setSelection(0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PAYMENT_REQUEST && data != null) {
+            Intent intent = new Intent(this, Resultado.class);
+            intent.putExtras(data.getExtras());
+            startActivity(intent);
+        }
+
+    }
+
+    public boolean isAvailable(Intent intent) {
+        final PackageManager mgr = getPackageManager();
+        List<ResolveInfo> list =
+                mgr.queryIntentActivities(intent,
+                        PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+}
---
+++ app/src/main/java/com/mercadopago/point/integration/basic/Resultado.java
@@ -0,0 +1,78 @@
+package com.mercadopago.point.integration.basic;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pgallazzi.apporonga.R;
+
+import java.util.Iterator;
+
+public class Resultado extends AppCompatActivity {
+
+    TextView textView;
+    TextView paymentId;
+    ImageView image;
+    View layoutView;
+
+    // it could be
+    public final static String RESULT_STATUS = "result_status";
+    public final static String RESULT_PAYMENT_ID = "paymentId";
+
+    public final static String RESULT_STATUS_OK = "OK";
+    public final static String RESULT_STATUS_FAILED = "FAILED";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+        textView = (TextView) findViewById(R.id.intent_results);
+        image = (ImageView) findViewById(R.id.icon);
+        layoutView = findViewById(R.id.payment_id_lo);
+        paymentId = (TextView) findViewById(R.id.payment_id);
+
+        Intent launcherIntent = getIntent();
+        Bundle data = launcherIntent.getExtras();
+        if (data != null) {
+
+            String result = data.getString(RESULT_STATUS);
+
+            if (RESULT_STATUS_OK.equals(result)) {
+                if (android.os.Build.VERSION.SDK_INT >= 22) {
+                    image.setImageDrawable(getDrawable(R.drawable.ok));
+                } else {
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.ok));
+                }
+
+                layoutView.setVisibility(View.VISIBLE);
+                paymentId.setText(data.getString(RESULT_PAYMENT_ID));
+            }
+
+            if (RESULT_STATUS_FAILED.equals(result)) {
+                if (android.os.Build.VERSION.SDK_INT >= 22) {
+                    image.setImageDrawable(getDrawable(R.drawable.fail));
+                } else {
+                    image.setImageDrawable(getResources().getDrawable(R.drawable.fail));
+                }
+                layoutView.setVisibility(View.GONE);
+            }
+
+            Iterator keys = data.keySet().iterator();
+            StringBuffer stringBuffer = new StringBuffer();
+            while (keys.hasNext()) {
+                String key = (String) keys.next();
+                stringBuffer.append(key + " " + String.valueOf(data.get(key)));
+            }
+            textView.setText(stringBuffer.toString());
+        } else
+
+        {
+            // won't happen.
+        }
+
+
+    }
+}
