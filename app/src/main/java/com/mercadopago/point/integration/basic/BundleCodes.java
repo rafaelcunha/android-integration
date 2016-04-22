2eece2873940c11f5b5d596482d896d4a18cc4ff
Sebastian Dominguez <sedominguez78@gmail.com>
2016-04-22 14:55:03 -0300

    Cambios MD

---
+++ app/src/main/java/com/mercadopago/point/integration/basic/BundleCodes.java
@@ -0,0 +1,21 @@
+package com.mercadopago.point.integration.basic;
+
+/**
+ * Created by sebad on 3/20/15.
+ */
+public interface BundleCodes {
+
+    String AMOUNT = "amount";
+    String DESCRIPTION = "description";
+    String RECEIPT_RECEIVER = "receiver";
+    String NEWS_HTML = "news";
+    String CARD_TYPE = "card_type";
+    String INSTALLMENTS = "installments";
+    String APP_ID = "app_id";
+    String APP_SECRET = "app_secret";
+    String APP_FEE = "app_fee";
+
+    String URL_SUCCESS = "url_success";
+    String URL_FAIL = "url_fail";
+
+}
--- app/src/main/java/com/mercadopago/point/integration/basic/Main.java
+++ app/src/main/java/com/mercadopago/point/integration/basic/Main.java
@@ -22,15 +22,6 @@ import java.util.List;
 public class Main extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-    public static final String LAUNCHER_INTENT_DESCRIPTION = "launcher_intent_description";
-    public static final String LAUNCHER_INTENT_AMOUNT = "launcher_intent_amount";
-    public static final String LAUNCHER_INTENT_TYPE = "launcher_intent_type";
-    public static final String LAUNCHER_INTENT_INSTALLMENTS = "launcher_intent_installments";
-
-    public static final String LAUNCHER_APP_ID = "launcher_app_id";
-    public static final String LAUNCHER_APP_SECRET = "launcher_app_secret";
-    public static final String LAUNCHER_APP_FEE = "launcher_app_fee";
-
     public static final int PAYMENT_REQUEST = 1;
 
     EditText reference;
@@ -66,28 +57,30 @@ public class Main extends AppCompatActivity implements AdapterView.OnItemSelecte
             public void onClick(View v) {
                 Intent i = new Intent();
                 i.setAction("com.mercadopago.PAYMENT_ACTION");
+                Bundle bundle = new Bundle();
                 // AppId
-                i.putExtra(LAUNCHER_APP_ID, appId);
+                bundle.putString(BundleCodes.APP_ID, appId);
                 // Secret
-                i.putExtra(LAUNCHER_APP_SECRET, appSecret);
+                bundle.putString(BundleCodes.APP_SECRET, appSecret);
                 // App Fee
-                i.putExtra(LAUNCHER_APP_FEE, appFee);
+                bundle.putDouble(BundleCodes.APP_FEE, appFee);
                 // Amount of transaction
-                i.putExtra(LAUNCHER_INTENT_AMOUNT, amount.getText().toString());
+                bundle.putDouble(BundleCodes.AMOUNT, Double.valueOf(amount.getText().toString()));
                 // Description of transaction
-                i.putExtra(LAUNCHER_INTENT_DESCRIPTION, reference.getText().toString());
+                bundle.putString(BundleCodes.DESCRIPTION, reference.getText().toString());
                 if (spinner.getSelectedItemPosition() == 0) {
                     cc_selected = "credit_card";
                 } else {
                     cc_selected = "debit_card";
                 }
                 // Payment type of transaction ( credit_card | debit_card  )
-                i.putExtra(LAUNCHER_INTENT_TYPE, cc_selected);
+                bundle.putString(BundleCodes.CARD_TYPE, cc_selected);
                 // # of installments
-                i.putExtra(LAUNCHER_INTENT_INSTALLMENTS, installments.getText().toString());
+                bundle.putInt(BundleCodes.INSTALLMENTS, Integer.valueOf(installments.getText().toString()));
                 // Before we can start the intent, we should check if this phone handle the intent?
                 if (isAvailable(i)) {
                     // start activity for result
+                    i.putExtras(bundle);
                     startActivityForResult(i, PAYMENT_REQUEST);
                 } else {
                     // send to google play.
